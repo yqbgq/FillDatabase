f015e0a8f842d177de2ee1f28f91c6f2a5d6635b
huangwei <446296992@qq.com>
2019-01-04 21:03:22 +0800

    新增抽象类AbstractClient，它持有了核心配置类，并且增加了Setter以及Getter方法，减少了在TaskClient中看到的和主要逻辑弱相关的代码

---
+++ src/main/java/application/AbstractClient.java
@@ -0,0 +1,25 @@
+package application;
+
+import properties.CoreProperty;
+
+/**
+ * 该抽象类持有了核心配置类，并且增加了Getter以及Setter方法
+ * 方便从配置类中读取信息，减少了在TaskClient中看到的和主要逻辑弱相关的代码
+ *
+ * @author 黄伟
+ */
+public abstract class AbstractClient {
+    /**持有的核心配置类对象**/
+    private CoreProperty property = new CoreProperty();
+
+    //------------Setter and Getter--------------------------
+    public void setUsername(String username){this.property.setUsername(username);}
+    public void setPassword(String password){this.property.setPassword(password);}
+    public void setUrlPrefix(String urlPrefix){this.property.setUrlPrefix(urlPrefix);}
+    public void setUrlSuffix(String urlSuffix){this.property.setUrlSuffix(urlSuffix);}
+    public String getUsername(){return this.property.getUsername();}
+    public String getPassword(){return this.property.getPassword();}
+    public String getUrlPrefix(){return this.property.getUrlPrefix();}
+    public String getUrlSuffix(){return this.property.getUrlPrefix();}
+    //------------Setter and Getter--------------------------
+}
