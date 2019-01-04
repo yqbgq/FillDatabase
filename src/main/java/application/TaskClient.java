b7ebd1595724ef7220f965794704590cf2626385
huangwei <446296992@qq.com>
2019-01-04 21:04:24 +0800

    新增TaskClient的继承关系,它继承了AbstractClient

--- src/main/java/application/TaskClient.java
+++ src/main/java/application/TaskClient.java
@@ -4,10 +4,12 @@ import task.Task;
 
 import java.util.ArrayList;
 
-public class TaskClient{
+public class TaskClient extends AbstractClient{
     /**持有的临时任务列表，确保在构建时较快，在start()方法被调用之后转换成Task对象列表**/
     private ArrayList<String> tempTaskList = new ArrayList<>();
 
     /**持有的任务列表，在start()方法被调用之后从tempTaskList转化而来，用来保存任务的相关信息**/
     private ArrayList<Task> TaskList = new ArrayList<>();
+
+    
 }
