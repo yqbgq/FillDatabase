76b9acf076351572e2801f333581265115dc93e7
huangwei <446296992@qq.com>
2019-01-04 20:46:16 +0800

    新建类CoreProperty类，它持有一些核心配置信息

---
+++ src/main/java/properties/CoreProperty.java
@@ -0,0 +1,21 @@
+package properties;
+
+/**
+ * 核心配置类，持有和数据库相关的一些配置信息
+ *
+ * @author 黄伟
+ *
+ */
+public class CoreProperty {
+    /**数据库的用户名**/
+    private String username;
+
+    /**数据库的密码**/
+    private String password;
+
+    /**URL连接的前缀**/
+    private String urlPrefix;
+
+    /**URL连接的后缀**/
+    private String urlSuffix;
+}
