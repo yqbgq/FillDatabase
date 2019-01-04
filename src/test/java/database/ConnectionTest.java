efd05bb1beb14327522852850d76aee4020b07f1
huangwei <446296992@qq.com>
2019-01-04 20:39:01 +0800

    新增一个测试类ConnectionTest，测试数据库连接

---
+++ src/test/java/database/ConnectionTest.java
@@ -0,0 +1,21 @@
+package database;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+    @Test
+    public void getConnection(){
+        String url = "jdbc:mysql://127.0.0.1:3306/java?useSSL=true&serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
