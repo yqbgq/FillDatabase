3d15d3d0e3db9d8ee3dd6052509be5c300e7a07c
huangwei <446296992@qq.com>
2019-01-04 20:51:53 +0800

    TaskClient类新增两个任务列表，用于存储临时和被转化的任务

--- src/main/java/application/TaskClient.java
+++ src/main/java/application/TaskClient.java
@@ -1,4 +1,13 @@
 package application;
 
-public class TaskClient {
+import task.Task;
+
+import java.util.ArrayList;
+
+public class TaskClient{
+    /**持有的临时任务列表，确保在构建时较快，在start()方法被调用之后转换成Task对象列表**/
+    private ArrayList<String> tempTaskList = new ArrayList<>();
+
+    /**持有的任务列表，在start()方法被调用之后从tempTaskList转化而来，用来保存任务的相关信息**/
+    private ArrayList<Task> TaskList = new ArrayList<>();
 }
