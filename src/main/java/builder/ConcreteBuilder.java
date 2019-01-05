ca68af8b2b4404e6ab41c4bea52868d3e2da0e49
huangwei <446296992@qq.com>
2019-01-05 20:16:39 +0800

    去除无意义代码

--- src/main/java/builder/ConcreteBuilder.java
+++ src/main/java/builder/ConcreteBuilder.java
@@ -7,10 +7,6 @@ import application.TaskClient;
  */
 public class ConcreteBuilder implements Builder {
     private TaskClient client = new TaskClient();
-
-    //不允许使用通过new进行初始化
-    private ConcreteBuilder(){}
-
     /**
      * 使用此方法对之前完成的构建方法进行校验，确认必要信息已经填写
      * 如果校验通过则返回经过构建的TaskClient的对象
