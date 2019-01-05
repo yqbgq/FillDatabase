8488fd6346e0679c6bd063b7724f7646f4199c71
huangwei <446296992@qq.com>
2019-01-05 20:09:53 +0800

    完善接口Builder以及注释

--- src/main/java/builder/Builder.java
+++ src/main/java/builder/Builder.java
@@ -1,4 +1,21 @@
 package builder;
 
+import application.TaskClient;
+
+/**
+ * ConcreteBuilder的接口，规定了其中必须实现的一些方法
+ *
+ * @author 黄伟
+ *
+ */
 public interface Builder {
+    TaskClient build();
+
+    Builder setUrlPrefix();
+
+    Builder setUrlSuffix();
+
+    Builder setPassword();
+
+    Builder setUsername();
 }
