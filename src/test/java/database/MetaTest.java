2335ed71ffda3af1f242c187380d70eb96bb1abd
huangwei <446296992@qq.com>
2019-01-07 17:35:44 +0800

    完成测试类，可以获取数据库中所有表名以及数据表中所有字段信息

--- src/test/java/database/MetaTest.java
+++ src/test/java/database/MetaTest.java
@@ -24,4 +24,29 @@ public class MetaTest {
             e.printStackTrace();
         }
     }
+
+    //测试获取每一列信息
+    @Test
+    public void getField(){
+        String url = "jdbc:mysql://127.0.0.1:3306/java?useSSL=true&serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            DatabaseMetaData m_DBMetaData = con.getMetaData();
+            ResultSet colRet = m_DBMetaData.getColumns(null,"%", "course","%");
+            while(colRet.next()) {
+                String columnName = colRet.getString("COLUMN_NAME");
+                String columnType = colRet.getString("TYPE_NAME");
+                int datasize = colRet.getInt("COLUMN_SIZE");
+                int digits = colRet.getInt("DECIMAL_DIGITS");
+                int nullable = colRet.getInt("NULLABLE");
+                System.out.println(columnName+" "+columnType+" "+datasize+" "+digits+" "+
+                        nullable);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
