7def2ec2d541533df1f39f9a788e1ca8e6fa9502
huangwei <446296992@qq.com>
2019-01-07 17:55:54 +0800

    bug fix

--- src/main/java/task/ATask.java
+++ src/main/java/task/ATask.java
@@ -1,5 +1,7 @@
 package task;
 
+import types.Type;
+
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,7 +18,7 @@ public abstract class ATask {
     /**任务需要写入的行数**/
     private AtomicInteger numOfRows;
     /**任务指向的表每一列的属性**/
-    private ArrayList<String> col ;
+    private ArrayList<Type> col ;
 
     //----------Getter and Setter----------------
     public String getDatabase() { return database; }
@@ -25,7 +27,7 @@ public abstract class ATask {
     public void setTable(String table) { this.table = table; }
     public AtomicInteger getNumOfRows() { return numOfRows; }
     public void setNumOfRows(AtomicInteger numOfRows) { this.numOfRows = numOfRows; }
-    public ArrayList<String> getCol() { return col; }
-    public void setCol(ArrayList<String> col) { this.col = col; }
+    public ArrayList<Type> getCol() { return col; }
+    public void setCol(ArrayList<Type> col) { this.col = col; }
     //----------Getter and Setter----------------
 }
