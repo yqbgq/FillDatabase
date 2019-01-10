f44466c8eddc604e167377a9663f06e5fb551e47
huangwei <446296992@qq.com>
2019-01-10 20:14:45 +0800

    完成SQL生成器

--- src/main/java/produce/SQLProduce.java
+++ src/main/java/produce/SQLProduce.java
@@ -15,13 +15,21 @@ public class SQLProduce {
         for(Type temp : task.getCol()){
             if(temp.getType().equals("int")){
                 if(!((Int)temp).isAutoIncrease()){
-                    prefixSql = prefixSql +  temp.getName() + ",";
-                    suffixSql = suffixSql + IntProduce.produce(temp.getLength()) + ",";
+                    prefixSql = prefixSql +  temp.getName() ;
+                    suffixSql = suffixSql + IntProduce.produce(temp.getLength()) ;
                 }
             }else{
+                prefixSql = prefixSql +  temp.getName() + ",";
+                suffixSql = suffixSql + IntProduce.produce(temp.getLength()) + ",";
 
             }
+            if(task.getCol().indexOf(temp) != task.getCol().size()-1){
+                prefixSql += ",";
+                suffixSql += ",";
+            }
         }
+        prefixSql += ")";
+        suffixSql += ")";
 
         return prefixSql + suffixSql;
     }
